05ef11b8ba5df1b4063bfa5ba06980e0f170a423
kaidarka <psse4sspe@gmail.com>
2020-09-24 16:18:53 +0300

    сделал адаптив для планшета

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,20 +1,6 @@
-$(function() {
-  var $window = $(window);
-  var $sidebar = $(".sidebar");
-  var $sidebarTop = $sidebar.position().top;
-  var $sidebarHeight = $sidebar.height();
-  var $footer = $('.footer');
-  var $footerTop = $footer.position().top;
-
-  $window.scroll(function(event) {
-    $sidebar.addClass("fixed");
-    var $scrollTop = $window.scrollTop();
-    var $topPosition = Math.max(0, $sidebarTop - $scrollTop);
-    
-    if ($scrollTop + $sidebarHeight > $footerTop) {
-      var $topPosition = Math.min($topPosition, $footerTop - $scrollTop - $sidebarHeight);
-    }
-
-    $sidebar.css("top", $topPosition);
-  });
+jQuery(document).ready(function() {
+    jQuery('.content, .sidebar').theiaStickySidebar({
+        // Настройки
+        additionalMarginTop: 30
+    });
 });
