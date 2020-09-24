db61bb1eb23755c3495a4661b8300e2ff6aa150d
kaidarka <psse4sspe@gmail.com>
2020-09-24 22:39:40 +0300

    сделать адаптив для телефона

---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+$("#header").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".header").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+$("#about").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".about").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+$("#projects").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".projects").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+$("#contacts").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".contacts").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+$("#blog").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".blog").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
